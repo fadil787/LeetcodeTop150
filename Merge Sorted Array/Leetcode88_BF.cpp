cf1501f98d4ace804db793d3e3d5b3ea5a888256
Mohammed Fadil Sayeed <141955582+fadil787@users.noreply.github.com>
2025-08-14 05:45:32 +0530

    Create Leetcode88_BF.cpp
    Contains the brute force solution of the problem. Detailed explanation is given in the README file of this problem. 
    
    NOTE: The solution contains only the function that is to be completed and not the entire code.

---
+++ Merge Sorted Array/Leetcode88_BF.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=0,j=0,k=0;
+
+        vector<int>nums1Copy = nums1;
+        while(i<m && j<n){
+            if(nums1Copy[i] <= nums2[j]){
+                nums1[k++] = nums1Copy[i++];
+            }
+            else{
+            nums1[k++] = nums2[j++];
+            }
+        
+        }
+        while(i<m){
+            nums1[k++] = nums1Copy[i++];
+        }
+        while(j<n){
+            nums1[k++] = nums2[j++];       }
+
+        
+    }
+};
