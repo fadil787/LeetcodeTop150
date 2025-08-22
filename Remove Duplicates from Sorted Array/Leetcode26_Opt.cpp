73f7c793836182ee61e49b78d0b6f98cf8f23fd4
Mohammed Fadil Sayeed <141955582+fadil787@users.noreply.github.com>
2025-08-22 11:17:18 +0530

    Update Leetcode26_Opt.cpp

--- Remove Duplicates from Sorted Array/Leetcode26_Opt.cpp
+++ Remove Duplicates from Sorted Array/Leetcode26_Opt.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int uniqueValIndex = 1; //this index keeps track of only the unique values
+        int size = nums.size();
+        for(int i=1; i<size; i++){
+            if(nums[i] != nums[i-1]){
+                nums[uniqueValIndex++] = nums[i];
+            }
+        }
+        return uniqueValIndex; //returns number of unique values present in nums
 
+        
+    }
+};
