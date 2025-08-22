087f025530c0cf1b36bb78b7a972b7add75833d0
Mohammed Fadil Sayeed <141955582+fadil787@users.noreply.github.com>
2025-08-22 11:36:48 +0530

    Update Leetcode26_BF.cpp

--- Remove Duplicates from Sorted Array/Leetcode26_BF.cpp
+++ Remove Duplicates from Sorted Array/Leetcode26_BF.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // Brute Force Approach using erase
+        for (int i = 1; i < nums.size(); ) {
+            if (nums[i] == nums[i - 1]) {
+                // Erase the duplicate element
+                nums.erase(nums.begin() + i);
+                // Don't increment i here, since elements shift left
+            } else {
+                i++; // Only move forward when no erase happens
+            }
+        }
+        return nums.size(); // Final size = number of unique elements
+    }
+};
 
