bcf1b4b1f18191977afc710816a71399ed128a02
Mohammed Fadil Sayeed <141955582+fadil787@users.noreply.github.com>
2025-08-22 10:19:33 +0530

    Update Leetcode27_Opt.cpp

--- Remove Element/Leetcode27_Opt.cpp
+++ Remove Element/Leetcode27_Opt.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int size = nums.size();
+        int nonValIndex = 0; // this is the index where we store elements which are not val
+        for(int i=0; i<size; i++){
+            if(nums[i] != val){
+                nums[nonValIndex++] = nums[i]; 
+            }
+        }
 
+        return nonValIndex; // this returns the number of elements which are not val
+
+        
+    }
+};
