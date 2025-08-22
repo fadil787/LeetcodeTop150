c6961ee98d264dc3714f0d5b6fc8cf516a164e14
Mohammed Fadil Sayeed <141955582+fadil787@users.noreply.github.com>
2025-08-22 15:50:09 +0530

    Update Leetcode80_Opt.cpp

--- Remove Duplicates from Sorted Array II/Leetcode80_Opt.cpp
+++ Remove Duplicates from Sorted Array II/Leetcode80_Opt.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int uniqueValIndex = 2;
+        int size = nums.size();
 
+        if(size <= 2) return size;
+
+        for(int i=2; i<size; i++){
+            if(nums[i] != nums[uniqueValIndex-2]){
+                nums[uniqueValIndex++] = nums[i];
+            }
+        }
+
+        return uniqueValIndex;
+        
+    }
+};
