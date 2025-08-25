ee5f47fedd3f0b0eacd425c70c005d572da22cb9
Mohammed Fadil Sayeed <141955582+fadil787@users.noreply.github.com>
2025-08-25 18:10:42 +0530

    Update Leetcode121_Opt.cpp

--- Best Time to Buy and Sell Stock/Leetcode121_Opt.cpp
+++ Best Time to Buy and Sell Stock/Leetcode121_Opt.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int size = prices.size();
+        int buyPrice = prices[0];
+        int currProfit = 0;
+        int maxProfit = 0;
 
+
+        for(int i=1; i<size; i++){
+            if(prices[i]<buyPrice) buyPrice = prices[i];
+            else{
+                currProfit = prices[i]-buyPrice;
+                maxProfit = max(maxProfit, currProfit);
+            }
+        }
+
+        return maxProfit;
+
+    
+        
+    }
+};
