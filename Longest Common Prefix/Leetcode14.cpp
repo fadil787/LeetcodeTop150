067adf838eac39537151cce4d02aaa41400623e7
Mohammed Fadil Sayeed <141955582+fadil787@users.noreply.github.com>
2025-09-04 17:44:27 +0530

    Update Leetcode14.cpp

--- Longest Common Prefix/Leetcode14.cpp
+++ Longest Common Prefix/Leetcode14.cpp
@@ -3,7 +3,7 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
         string output = "";
-        sort(strs.begin(),strs.end());  //lexographically sorted
+        sort(strs.begin(),strs.end());  //lexicographically sorted
 
         string first = strs[0];
         string last = strs[n-1];
