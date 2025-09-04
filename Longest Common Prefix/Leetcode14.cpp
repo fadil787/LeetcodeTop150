a491e585c865c6595f11fe88233ccb9bd1ec3eed
Mohammed Fadil Sayeed <141955582+fadil787@users.noreply.github.com>
2025-09-04 17:40:50 +0530

    Create Leetcode14.cpp

---
+++ Longest Common Prefix/Leetcode14.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        string output = "";
+        sort(strs.begin(),strs.end());  //lexographically sorted
+
+        string first = strs[0];
+        string last = strs[n-1];
+
+        for(int i=0; i<first.length(); i++){
+            if(first[i] == last[i]) output+=first[i];
+            else break;
+        }
+
+        return output;
+    }
+};
