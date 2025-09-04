195748ce605d23e2c0ebacdca5e401ba572ae498
Mohammed Fadil Sayeed <141955582+fadil787@users.noreply.github.com>
2025-09-04 19:36:17 +0530

    Create Leetcode151_Twopointer.cpp

---
+++ Reverse Words in a String/Leetcode151_Twopointer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+
+        int n = s.size();
+        int i = 0;
+        int l = 0, r = 0;
+
+        while (i < n) {
+            while (i < n && s[i] != ' ') {
+                s[r++] = s[i++];
+            }
+
+            if (l < r) {
+                reverse(s.begin() + l, s.begin() + r);
+                s[r] = ' ';
+                r++;
+                l = r;
+            }
+            i++;
+        }
+
+        return s.substr(0, r - 1);
+    }
+};
